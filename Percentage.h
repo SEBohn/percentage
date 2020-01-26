a76842f71df2857b05d977d7783f7a29381df2d8
Jinnis <bohnsarahe@gmail.com>
2020-01-26 16:16:57 -0500

    Add files via upload

---
+++ Percentage.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+// variables for monthly pay and the contribution.
+double monthlyPay = 6000.0, contribution;
+
+//calculation and display of the 5% contribution.
+contribution = monthlyPay * 0.05;
+
+cout << "5 percent is $" << contribution << " per month.\n";
+
+//calculation and display of the 7% contribution.
+contribution = monthlyPay * 0.07;
+
+cout << "7 percent is $" << contribution << " per month.\n";
+
+//calculation and display of the 10% contribution.
+contribution = monthlyPay * 0.10;
+
+cout << "10 percent is $" << contribution << " per month.\n";
+
+return 0;
+
+}
